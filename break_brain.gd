bf571b57fee81291fb501adada269093022d2c7a
SpaceGirlCeres_Celeste.py <80549140+SpaceGirlCeres@users.noreply.github.com>
2025-08-11 18:56:12 -0400

    math
    added _pow, _log
    changed some variable names
    added coments
    added large_float

--- break_brain.gd
+++ break_brain.gd
@@ -1,4 +1,15 @@
+#
+# Thanks To My Best Friend Shrek For All The Help
+#
+
+#
+# Heavily inspired off of break_eternity.gd by Minemaker0430*
+# https://github.com/Minemaker0430/break_eternity.gd
+# *significantly less error handling beacause this is intended for my personal use
+#
+
 class_name Large
+# Large has support for numbers up to ee308
 
 var man := 0.0
 var power = 0
@@ -8,6 +19,7 @@ func _init(_man: float, _power) -> void:
 	power = _power
 	self.normalize()
 	
+	
 func large(mantissa, exponent) -> Large:
 	return Large.new(mantissa, exponent)
 
@@ -16,12 +28,17 @@ func normalize() -> Large:
 		if power != INF:
 			power = 0
 	elif ! (1 <= abs(man) and abs(man) < 10):
-		var norm = floor(Math.loger(abs(man)))
+		var norm = floor(Math.loger(abs(man))+0.0000000000001)
 		power += norm
 		man /= 10**norm
 	return self
 		
-func add(num2: Large) -> Large:
+func _round(dec: int) -> Large:
+	return large(Math.rounded(man, dec), Math.rounded(power, 1))
+		
+		
+# simple math functions
+func _add(num2: Large) -> Large:
 	var num1 = self
 	if num1.power < num2.power:
 		var temp = num2
@@ -31,16 +48,16 @@ func add(num2: Large) -> Large:
 	num1.man += num2.man / 10**(num1.power-num2.power)
 	return num1.normalize()
 	
-func sub(num2: Large) -> Large:
-	return add(num2.neg())
+func _sub(num2: Large) -> Large:
+	return _add(num2.neg())
 	
-func mult(num2 : Large) -> Large:
+func _mult(num2 : Large) -> Large:
 	var num1 = self
 	num1.man *= num2.man
 	num1.power += num2.power
 	return num1.normalize()
 
-func div(num2 : Large) -> Large:
+func _div(num2 : Large) -> Large:
 	var num1 = self
 	if !num2.man:
 		if num1.man:
@@ -51,6 +68,17 @@ func div(num2 : Large) -> Large:
 	num1.power -= num2.power
 	return num1.normalize()
 	
+	
+# complex math
+func _log(base: Large) -> Large:
+	return large((Math.loger(man)+power)/(Math.loger(base.man)+base.power), 0).normalize()
+	
+func _pow(exponent: Large) -> Large:
+	var e = exponent.large_float()
+	return large(man**e*10**(fmod(power*e, 1)),floor(power*e)).normalize()
+	
+	
+# comparisons
 func is_equ(num2: Large) -> bool:
 	var num1 = self
 	return (num1.man == num2.man) and (num1.power == num2.power)
@@ -85,6 +113,19 @@ func less_equ(num2: Large) -> bool:
 	var num1 = self
 	return num1.is_equ(num2) or num1.is_less(num2)
 
+
+# large to other type
 func large_str() -> String:
-	return str(man, "e", power)
+	if abs(power) >= 1e3:
+		return str(man, "e", large(power, 0).man, "e", large(power, 0).power)
+	elif power >= 3:
+		return str(man, "e", power)
+	else:
+		return str(man*10**power)
+	
+func large_float() -> float:
+	if power < 308:
+		return float(man*10**power)
+	else:
+		return INF
 	
